7ef35987c8dca295f0373c02905a1feead60a626
yunus emre kara <yunusemrejs@outlook.com>
2023-04-15 02:18:32 +0300

    Refactor notify function for improve performance

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,45 @@
 /* eslint-disable @typescript-eslint/ban-types */
-interface StoreOptions<State> {
-  state?: State;
+interface StoreOptions<State extends Record<string, any>> {
+  state: State;
   actions?: Actions<State>;
   mutations?: Mutations<State>;
   middlewares?: Middleware<State>[];
 }
 
-interface Actions<State> {
+interface Actions<State extends Record<string, any>> {
   [key: string]: (store: ReStore<State>, payload?: any) => any;
 }
 
-interface Mutations<State> {
+interface Mutations<State extends Record<string, any>> {
   [key: string]: (state: State, payload?: any) => void;
 }
 
-interface Middleware<State> {
+interface Middleware<State extends Record<string, any>> {
   (context: MiddlewareContext<State>): (next: Function) => any;
 }
 
-interface MiddlewareContext<State> {
+interface MiddlewareContext<State extends Record<string, any>> {
   store: ReStore<State>;
   actionName: string;
   payload?: any;
 }
 
-class ReStore<State> {
+interface Listener<State extends Record<string, any>> {
+  watchedStates: (keyof State)[];
+  callback: (state: State) => void;
+}
+
+class ReStore<State extends Record<string, any>> {
   private state: State;
   private actions: Actions<State>;
   private mutations: Mutations<State>;
   private middlewares: Middleware<State>[];
-  private listeners: Function[];
+  private listeners: Listener<State>[];
   private queuedListeners: Function[];
   private isUpdating: boolean;
 
-  constructor(options: StoreOptions<State> = {}) {
-    const { state = {} as State, actions = {}, mutations = {}, middlewares = [] } = options;
+  constructor(options: StoreOptions<State>) {
+    const { state, actions = {}, mutations = {}, middlewares = [] } = options;
     this.state = Object.freeze(state);
     this.actions = actions;
     this.mutations = mutations;
@@ -64,16 +69,21 @@ class ReStore<State> {
     }
   }
 
-  public subscribe(listener: Function): void {
+  public subscribe(listener: Listener<State>): void {
     this.listeners.push(listener);
   }
 
-  public unsubscribe(listener: Function): void {
+  public unsubscribe(listener: Listener<State>): void {
     this.listeners = this.listeners.filter((l) => l !== listener);
   }
 
-  public notify(): void {
-    this.listeners.forEach((listener) => listener());
+  public notify(changedKeys: Array<keyof State> = []): void {
+    this.listeners.forEach((listener) => {
+      if(!changedKeys.length) listener.callback(this.state);
+      if (listener.watchedStates.length === 0 || listener.watchedStates.some((key) => changedKeys.includes(key))) {
+        listener.callback(this.state);
+      }
+    });
   }
 
   public async dispatch(actionName: string, payload?: any): Promise<any> {
@@ -94,15 +104,17 @@ class ReStore<State> {
   public async commit(mutationName: string, payload?: any): Promise<void> {
     const mutation = this.mutations[mutationName];
     if (mutation) {
+      const previousState = this.state;
       await mutation(this.state, payload);
-      this.notify();
+      const changedKeys = Object.keys(this.state).filter((key) => this.state[key] !== previousState[key]) as (keyof State)[];
+      this.notify(changedKeys);
     } else {
       console.error(`Mutation '${mutationName}' not found.`);
     }
   }
 }
 
-function createStore<State>(options: StoreOptions<State>): ReStore<State> {
+function createStore<State extends Record<string, any>>(options: StoreOptions<State>): ReStore<State> {
   return new ReStore(options);
 }
 
