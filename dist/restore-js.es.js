80c2f2b810693997285328449a890907a7955080
yunus emre kara <yunusemrejs@outlook.com>
2023-04-15 05:49:41 +0300

    Export all types for typescript support

--- dist/restore-js.es.js
+++ dist/restore-js.es.js
@@ -1,48 +1,18 @@
-/* eslint-disable @typescript-eslint/ban-types */
-interface StoreOptions {
-  state: State;
-  actions?: Actions;
-  mutations?: Mutations;
-  middlewares?: Middleware[];
-}
-
-interface State {
-  [key: string]: any;
-}
-
-interface Actions {
-  [key: string]: (store: ReStore, payload?: any) => any;
-}
-
-interface Mutations {
-  [key: string]: (state: State, payload?: any) => void;
-}
-
-interface Middleware {
-  (context: MiddlewareContext): (next: Function) => any;
-}
-
-interface MiddlewareContext {
-  store: ReStore;
-  actionName: string;
-  payload?: any;
-}
-
-interface Listener {
-  watchedStates: (keyof State)[];
-  callback: (state: State) => void;
-}
-
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __publicField = (obj, key, value) => {
+  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+  return value;
+};
 class ReStore {
-  private state: State;
-  private actions: Actions;
-  private mutations: Mutations;
-  private middlewares: Middleware[];
-  private listeners: Listener[];
-  private queuedListeners: Function[];
-  private isUpdating: boolean;
-
-  constructor(options: StoreOptions) {
+  constructor(options) {
+    __publicField(this, "state");
+    __publicField(this, "actions");
+    __publicField(this, "mutations");
+    __publicField(this, "middlewares");
+    __publicField(this, "listeners");
+    __publicField(this, "queuedListeners");
+    __publicField(this, "isUpdating");
     const { state, actions = {}, mutations = {}, middlewares = [] } = options;
     this.state = state;
     this.actions = actions;
@@ -52,12 +22,10 @@ class ReStore {
     this.queuedListeners = [];
     this.isUpdating = false;
   }
-
-  public getState(): State {
+  getState() {
     return this.state;
   }
-
-  public setState(state: State): void {
+  setState(state) {
     if (this.isUpdating) {
       this.queuedListeners.push(() => {
         this.state = state;
@@ -72,25 +40,22 @@ class ReStore {
       this.queuedListeners = [];
     }
   }
-
-  public subscribe(listener: Listener): void {
+  subscribe(listener) {
     this.listeners.push(listener);
   }
-
-  public unsubscribe(listener: Listener): void {
+  unsubscribe(listener) {
     this.listeners = this.listeners.filter((l) => l !== listener);
   }
-
-  public notify(changedKeys: Array<keyof State> = []): void {
+  notify(changedKeys = []) {
     this.listeners.forEach((listener) => {
-      if(!changedKeys.length) listener.callback(this.state);
+      if (!changedKeys.length)
+        listener.callback(this.state);
       if (listener.watchedStates.length === 0 || listener.watchedStates.some((key) => changedKeys.includes(key))) {
         listener.callback(this.state);
       }
     });
   }
-
-  public async dispatch(actionName: string, payload?: any): Promise<any> {
+  async dispatch(actionName, payload) {
     const action = this.actions[actionName];
     if (action) {
       const context = { store: this, actionName, payload };
@@ -104,23 +69,22 @@ class ReStore {
       console.error(`Action '${actionName}' not found.`);
     }
   }
-
-  public async commit(mutationName: string, payload?: any): Promise<void> {
+  async commit(mutationName, payload) {
     const mutation = this.mutations[mutationName];
     if (mutation) {
       const previousState = this.state;
       await mutation(this.state, payload);
-      const changedKeys = Object.keys(this.state).filter((key) => this.state[key] !== previousState[key]) as (keyof State)[];
+      const changedKeys = Object.keys(this.state).filter((key) => this.state[key] !== previousState[key]);
       this.notify(changedKeys);
     } else {
       console.error(`Mutation '${mutationName}' not found.`);
     }
   }
 }
-
-function createStore(options: StoreOptions): ReStore {
+function createStore(options) {
   return new ReStore(options);
 }
-
-export { ReStore, createStore };
-export type { StoreOptions, State, Actions, Mutations, Middleware, Listener };
+export {
+  ReStore,
+  createStore
+};
